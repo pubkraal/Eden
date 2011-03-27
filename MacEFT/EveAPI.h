640fe56b5deee9cfacfbbf6ec58487b6cc0a8758
John Kraal <jpakraal@gmail.com>
2011-03-27 19:55:20 +0200

    Several things done to get the API working.
    * Added skeleton for Eve API Layer programming
    * Added JSON for parsing the Eve API XML to JSON, for easy (read: not retarded) access of properties.
    * Added 2-clause BSD License for main app. JSON lib uses 3-clause, and that has been included in their files.

---
+++ MacEFT/EveAPI.h
@@ -0,0 +1,24 @@
+//
+//  EveAPI.h
+//  MacEFT
+//
+//  Created by John Kraal on 3/27/11.
+//  Copyright 2011 Netframe. All rights reserved.
+//
+
+/**
+ * Point of this is to fetch data from the Eve API provider and return the data
+ * in a JSON like manner. If Objective-C supports such a scheme (Python
+ * Dictionaries, where are you?).
+ */
+
+#import <Foundation/Foundation.h>
+
+// For use of this lib look at http://code.google.com/p/json-framework/
+#import "JSON.h"
+
+@interface EveAPI : NSObject {
+    
+}
+
+@end
---
+++ MacEFT/EveAPI.m
@@ -0,0 +1,14 @@
+//
+//  EveAPI.m
+//  MacEFT
+//
+//  Created by John Kraal on 3/27/11.
+//  Copyright 2011 Netframe. All rights reserved.
+//
+
+#import "EveAPI.h"
+
+
+@implementation EveAPI
+
+@end
