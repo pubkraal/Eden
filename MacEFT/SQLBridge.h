5f3889f604247ea5bc33455260bcd57679fc4057
ugo pozo <ugopozo@alignleft.net>
2011-04-20 05:40:52 -0300

    Started the implementation of SQLBridge, switched to default OS X libsqlite3.dylib, created dummy DB for testing.

---
+++ MacEFT/SQLBridge.h
@@ -0,0 +1,17 @@
+//
+//  SQLBridge.h
+//  MacEFT
+//
+//  Created by ugo pozo on 4/8/11.
+//  Copyright 2011 Netframe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SQLBridge : NSObject {
+@private
+    
+}
+
+@end
---
+++ MacEFT/SQLBridge.m
@@ -0,0 +1,29 @@
+//
+//  SQLBridge.m
+//  MacEFT
+//
+//  Created by ugo pozo on 4/8/11.
+//  Copyright 2011 Netframe. All rights reserved.
+//
+
+#import "SQLBridge.h"
+
+
+@implementation SQLBridge
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
